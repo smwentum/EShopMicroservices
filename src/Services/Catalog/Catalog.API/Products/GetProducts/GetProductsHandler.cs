afc71409329dafbbd4821b292db9a7c6e4a37a22
smwentum <sai.wentum@huskers.unl.edu>
2024-04-13 10:20:28 -0400

    added logging

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -11,8 +11,6 @@ internal class GetProductsQueryHandler
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
-
         var products = await session.Query<Product>().ToListAsync(cancellationToken);
 
         return new GetProductsResult(products);
