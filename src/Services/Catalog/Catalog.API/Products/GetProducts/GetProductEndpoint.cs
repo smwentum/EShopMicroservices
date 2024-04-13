359c4c151fdf9d3dcb23a8554127d2e0ef9e5503
smwentum <sai.wentum@huskers.unl.edu>
2024-04-13 10:59:50 -0400

    almost done with section 7

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace Catalog.API.Products.GetProducts;
 
+public record GetProductsRequests(int? pageNumber = 1, int? pageSize = 10);
 
 public record GetProductsResponse(IEnumerable<Product> Products);
 
@@ -9,9 +10,11 @@ public class GetProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] GetProductsRequests request, ISender sender) =>
         {
-            var result = await sender.Send(new GetProductsQuery());
+            var query = request.Adapt<GetProductsQuery>();
+
+            var result = await sender.Send(query);
 
             var response = result.Adapt<GetProductsResponse>();
 
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,7 +1,6 @@
-﻿
-namespace Catalog.API.Products.GetProducts;
+﻿namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? pageNumber = 1, int? pageSize = 10) : IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<Product> Products);
 
@@ -11,7 +10,8 @@ internal class GetProductsQueryHandler
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(query.pageNumber ?? 1, query.pageSize ?? 10, cancellationToken);
 
         return new GetProductsResult(products);
     }
